144e2a8f0f3b408eb4f97566307748c319b71a86
vivek-regmi-au5 <vivek3639@gmail.com>
2020-07-02 19:22:00 +0530

    production process

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 mongoose.connect(
   `mongodb+srv://vivek:${process.env.MONGODB_PASSWORD}@cluster0-va8yu.mongodb.net/phorekka?retryWrites=true&w=majority
@@ -36,6 +37,14 @@ app.use((err, req, res, next) => {
   console.log("error: ", err);
   res.send(err.message);
 });
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Starting the server
 const port = process.env.PORT || 9122;
