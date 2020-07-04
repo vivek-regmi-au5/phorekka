368eba6d419bb3ba1915f9e24aaf772554b16fc9
alfred-joseph-au5 <aj.oem.01@gmail.com>
2020-07-04 08:14:38 +0530

    Customizing goole button (TESTING!)

---
+++ client/src/components/Auth/GoogleBtn.jsx
@@ -0,0 +1,11 @@
+// TODO: Complete this component later
+import React from 'react'
+import Icon from '../../img/GglIcon';
+
+const GoogleBtn = (props) => {
+    return (
+        <button><Icon active={true} /> {props.text}</button>
+    )
+}
+
+export default GoogleBtn;
